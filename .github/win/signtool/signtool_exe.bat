5ae918245bd027596acc76f9de034b56ef8ec351
phuong <phuonghqh@gmail.com>
2021-05-06 16:01:46 +0700

    dwedwe

---
+++ .github/win/signtool/signtool_exe.bat
@@ -0,0 +1,14 @@
+REM path to signtool.exe
+IF EXIST "%ProgramFiles(x86)%\Windows Kits\10\bin\x64\signtool.exe" (
+    SET SIGNTOOL_EXE="%ProgramFiles(x86)%\Windows Kits\10\bin\x64\signtool.exe"
+)
+
+IF EXIST "%ProgramFiles(x86)%\Windows Kits\10\bin\x86\signtool.exe" (
+    SET SIGNTOOL_EXE="%ProgramFiles(x86)%\Windows Kits\10\bin\x64\signtool.exe"
+)
+
+IF EXIST "%ProgramFiles%\Microsoft SDKs\Windows\v7.1\Bin\signtool.exe" (
+    SET SIGNTOOL_EXE="%ProgramFiles%\Microsoft SDKs\Windows\v7.1\Bin\signtool.exe"
+)
+
+echo %SIGNTOOL_EXE%
