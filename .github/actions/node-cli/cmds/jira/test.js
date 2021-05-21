7ecd5138070dd5014e683dd36c0d85b90715e96a
phuong <phuonghqh@gmail.com>
2021-05-21 16:45:01 +0700

    dewdew

--- .github/actions/node-cli/cmds/jira/test.js
+++
@@ -1,9 +0,0 @@
-exports.command = 'test';
-exports.desc = 'test cmd for go-jira';
-
-const debug = require('debug')('JIRA');
-
-exports.handler = (argv) => {
-  const execSync = require('child_process').execSync;
-  debug(execSync(`${process.env.JIRA} help`).toString());
-};
